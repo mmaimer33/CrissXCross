5221370f9e658d7b86306c1224f9c25ecd59fb14
mmaimer33 <rchousalkar@gmail.com>
2022-05-29 20:02:48 +0530

    Connected front-end to back-end (server)

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route("/test")
 def index():
-    return "Something works"
+    return {"messages": ["Hi", "Hello", "There"]}
 
 if __name__ == "__main__":
     app.run()
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/test').then(response => {
+      console.log("SUCCESS", response)
+      setData(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React + Flask Tutorial</p>
+        <div>{data.status === 200 ? 
+          <h3>{data.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
       </header>
     </div>
   );
